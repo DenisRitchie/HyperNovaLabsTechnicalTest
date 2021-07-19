6b499da56625d04415a5e1e5dbcf3d336bdd8bc6
Denis West <deniswest@outlook.com>
2021-07-19 07:28:57 -0600

    Noesis GUI Configuration Library

--- HyperNovaLabs/CMake/FindNoesisGUI.cmake
+++ HyperNovaLabs/CMake/FindNoesisGUI.cmake
@@ -5,3 +5,24 @@
 # https://www.foonathan.net/2016/03/cmake-install/
 # https://stackoverflow.com/questions/24570916/add-external-libraries-to-cmakelist-txt-c
 # https://code.i-harness.com/en/docs/cmake~3.15/command/target_link_directories
+
+# https://leimao.github.io/blog/CMake-Public-Private-Interface/
+# https://dominikberner.ch/cmake-interface-lib/
+# https://steveire.wordpress.com/2016/08/09/opt-in-header-only-libraries-with-cmake/
+
+# https://cmake.org/cmake/help/latest/prop_gbl/CMAKE_CXX_KNOWN_FEATURES.html
+# target_compile_features(${PROJECT_NAME}
+#     PRIVATE cxx_std_20
+#   # PRIVATE cxx_std_11
+# )
+
+# https://cmake.org/cmake/help/latest/command/install.html
+
+# https://retifrav.github.io/blog/2021/03/08/cmake-cpp-library/
+# https://cmake.org/cmake/help/latest/command/project.html
+
+# https://stackoverflow.com/questions/3249459/for-the-cmake-include-command-what-is-the-difference-between-a-file-and-a-mod
+# set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH};${CMAKE_CURRENT_SOURCE_DIR}/CMake")
+
+# https://cmake.org/cmake/help/latest/command/configure_file.html
+# configure_file(Templates/index.html.in index.html @ONLY)
